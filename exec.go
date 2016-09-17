192851adb4a489c18fc13fd1673f8e3fa5467fed
Denis Barov <dindin@dindin.ru>
2016-09-18 06:48:07 +0700

    system command execution

---
+++ exec.go
@@ -0,0 +1,22 @@
+package ipmibot
+
+import (
+	//"fmt"
+	//"log"
+	"os/exec"
+)
+
+type execReturn struct {
+	output string
+	err    error
+}
+
+func execCommand(command string, args ...string) execReturn {
+	cmd := exec.Command(command, args...)
+	err := cmd.Wait()
+	stdout, err := cmd.CombinedOutput()
+	return execReturn{
+		output: string(stdout),
+		err:    err,
+	}
+}
