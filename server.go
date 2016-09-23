c6648a2b2a17cc44bcdf61fa0aab80e2a67d76a7
Denis Barov <dindin@dindin.ru>
2016-09-23 19:47:19 +0700

    more 'alias' subcommands

--- server.go
+++ server.go
@@ -12,22 +12,19 @@ import (
 	"syscall"
 )
 
-func ipmi(w http.ResponseWriter, r *http.Request) {
+func processIpmi(body []byte) string {
 
-	body, err := ioutil.ReadAll(r.Body)
 	data := parseInputJson(body)
-	_, _ = err, data
-
 	var logEntry dbLogEntry
 	var output string
 	var color string = "green"
 	var out execReturn
 
-	log.Printf("Command %s received from %s\n", data.command, data.name)
+	//log.Printf("Command %s received from %s\n", data.command, data.name)
 	switch data.command {
 	case "help":
 		output = help(data.args)
-		color = "blue"
+		color = "purple"
 	case "reboot", "on", "off", "lanboot", "status":
 		if len(data.args) == 0 || data.args[0] == "" {
 			output = help([]string{data.command})
@@ -36,23 +33,36 @@ func ipmi(w http.ResponseWriter, r *http.Request) {
 			out = IpmiExec(data.args[0], data.command)
 			output = out.output
 		}
-
 	case "alias":
 		switch {
 		case len(data.args) == 0:
-			aliasEntry := mkDbAliasEntry("", data.name, "")
+			aliasEntry := mkDbAliasEntry("", data.name, "0.0.0.0")
 			aliasEntries := showAlias(*aliasEntry)
 			output = fmt.Sprintf("Aliases for you:\n")
 			for _, a := range aliasEntries {
 				output += aliasToString(a)
+				output += "\n"
 			}
 		case len(data.args) == 2 && data.args[0] == "del":
-			output = data.args[0]
-		case len(data.args) == 2 && data.args[0] == "show":
-			aliasEntry := mkDbAliasEntry(data.args[1], data.name, "")
+			aliasEntry := mkDbAliasEntry(data.args[1], data.name, "0.0.0.0")
 			aliasEntries := showAlias(*aliasEntry)
 			if len(aliasEntries) == 0 {
-				output = fmt.Sprintf("There is no alias '%s' for %s\n", data.args[2], data.name)
+				output = fmt.Sprintf("There is no alias '%s' for %s\n", data.args[1], data.name)
+				color = "red"
+				break
+			} else {
+				delAlias(*aliasEntry)
+				aliasEntries := showAlias(*aliasEntry)
+				if len(aliasEntries) == 0 {
+					output = fmt.Sprintf("Alias '%s' removed (owner  %s)", data.args[1], data.name)
+				}
+			}
+		case len(data.args) == 2 && data.args[0] == "show":
+			aliasEntry := mkDbAliasEntry(data.args[1], data.name, "0.0.0.0")
+			aliasEntries := showAlias(*aliasEntry)
+			if len(aliasEntries) == 0 {
+				output = fmt.Sprintf("There is no alias '%s' for %s\n", data.args[1], data.name)
+				color = "red"
 				break
 			}
 			output = aliasToString(aliasEntries[0])
@@ -61,18 +71,19 @@ func ipmi(w http.ResponseWriter, r *http.Request) {
 			ip := net.ParseIP(data.args[2])
 			if ip == nil {
 				output = fmt.Sprintf("'%s' doesn't look like IP-address to me\n", data.args[2])
+				color = "red"
 				break
 			}
 			updateAlias(*aliasEntry)
 			aliasEntries := showAlias(*aliasEntry)
 			if len(aliasEntries) == 0 {
 				output = "Something wrong, I cannot add alias =("
+				color = "red"
 				break
 			}
 			output = aliasToString(aliasEntries[0])
 		default:
 			output = help([]string{data.command})
-			color = "purple"
 		}
 	}
 
@@ -86,6 +97,12 @@ func ipmi(w http.ResponseWriter, r *http.Request) {
 	logEntry.systemerror = out.err
 
 	logToDB(logEntry)
+	return output
+}
+
+func serveIpmi(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	output := processIpmi(body)
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, output)
 }
@@ -161,7 +178,7 @@ func main() {
 	}
 
 	mux = make(map[string]func(http.ResponseWriter, *http.Request))
-	mux["/ipmi"] = ipmi
+	mux["/ipmi"] = serveIpmi
 
 	server.ListenAndServe()
 	log.Println("Server listening on address", addr)
