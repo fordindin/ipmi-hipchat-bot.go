ace7e289afe59d4ab2b888fbaba5930f7b56470f
Denis Barov <dindin@bhyve-host-01.argotech.io>
2016-09-23 07:01:59 +0000

    move definitions, rework return valules

--- ipmi.go
+++ ipmi.go
@@ -1,37 +1,13 @@
 package main
 
-import (
-	//"fmt"
-	"os/exec"
-	"strings"
-)
-
-type ipmiExecResult struct {
-	command string
-	result  execReturn
-}
-
-var ipmiUserame string = "ADMIN"
-var ipmiPassword string = "ADMIN"
-
-var ipmitool, ipmitoolBinErr = exec.LookPath("ipmitool")
-
-var commands = map[string][]string{
-	"status": []string{"chassis", "power", "status"},
-	"off":    []string{"chassis", "power", "off"},
-	"on":     []string{"chassis", "power", "on"},
-	"cycle":  []string{"chassis", "power", "cycle"},
-}
-
-func IpmiExec(host string, command string) ipmiExecResult {
-	var ret ipmiExecResult
+func IpmiExec(host string, command string) execReturn {
+	var ret execReturn
 	var cmdArray []string
 	cmdArray = append(cmdArray,
 		"-U", ipmiUserame,
 		"-P", ipmiPassword,
 		"-H", host)
 	cmdArray = append(cmdArray, commands[command]...)
-	ret.command = strings.Join(cmdArray, " ")
-	ret.result = execCommand(ipmitool, cmdArray...)
+	ret = execCommand(ipmitool, cmdArray...)
 	return ret
 }
