849d523e0b8084f1b376d52435c2127a096f0074
Denis Barov <dindin@dindin.ru>
2016-09-26 13:48:40 +0700

    add daemonizing support

--- definitons.go
+++ definitons.go
@@ -1,9 +1,29 @@
 package main
 
-import "os/exec"
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os/exec"
+)
 
+var (
+	signal = flag.String("s", "", `send signal to the daemon
+		quit — graceful shutdown
+		stop — fast shutdown
+		reload — reloading the configuration file`)
+)
+
+var listener net.Listener
+
+var daemonName = "ipmi-hipchat-gobot"
 var address string = ""
 var port int = 8000
+
+var pidfile = fmt.Sprintf("%s.pid", daemonName)
+var logfile = fmt.Sprintf("%s.log", daemonName)
+var workdir = "."
+var processName []string = []string{fmt.Sprintf("[%s]", daemonName)}
 
 var ipmiUserame string = "ADMIN"
 var ipmiPassword string = "ADMIN"
--- server.go
+++ server.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/sevlyar/go-daemon"
 	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
-	"os/signal"
 	"syscall"
 )
 
@@ -25,11 +26,14 @@ func processIpmi(body []byte) string {
 	case "help":
 		output = help(data.args)
 		color = "purple"
+	case "last":
 	case "reboot", "on", "off", "lanboot", "status":
-		if len(data.args) == 0 || data.args[0] == "" {
+		switch {
+		case len(data.args) == 0:
 			output = help([]string{data.command})
 			color = "purple"
-		} else {
+		case data.args[1] == "lanboot":
+		default:
 			out = IpmiExec(data.args[0], data.command)
 			output = out.output
 		}
@@ -114,7 +118,6 @@ func (*httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		h(w, r)
 		return
 	}
-
 	io.WriteString(w, help(make([]string, 0)))
 }
 
@@ -158,28 +161,95 @@ Shows ip address for alias <alias name>
 	return helpstr
 }
 
-func main() {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM)
-	go func() {
-		sig := <-c
-		DB.Close()
-		log.Println("Signal received, exiting:", sig)
-		os.Exit(0)
-	}()
-
+func worker() {
 	var addr string
+	var err error
 	initDB()
 	addr = fmt.Sprintf("%s:%d", address, port)
-
-	server := http.Server{
-		Addr:    addr,
-		Handler: &httpHandler{},
+	listener, err = net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	mux = make(map[string]func(http.ResponseWriter, *http.Request))
-	mux["/ipmi"] = serveIpmi
+	/*
+		server = http.Server{
+			Addr:    addr,
+			Handler: &httpHandler{},
+		}
+	*/
 
-	server.ListenAndServe()
+	//mux = make(map[string]func(http.ResponseWriter, *http.Request))
+	//mux["/ipmi"] = serveIpmi
+	handler := http.NewServeMux()
+	handler.HandleFunc("/ipmi", serveIpmi)
+
+	http.Serve(listener, handler)
 	log.Println("Server listening on address", addr)
+}
+
+func main() {
+	flag.Parse()
+	daemon.AddCommand(daemon.StringFlag(signal, "quit"), syscall.SIGQUIT, termHandler)
+	daemon.AddCommand(daemon.StringFlag(signal, "stop"), syscall.SIGTERM, termHandler)
+	daemon.AddCommand(daemon.StringFlag(signal, "reload"), syscall.SIGHUP, reloadHandler)
+
+	cntxt := &daemon.Context{
+		PidFileName: pidfile,
+		PidFilePerm: 0644,
+		LogFileName: logfile,
+		LogFilePerm: 0640,
+		WorkDir:     workdir,
+		Umask:       027,
+		Args:        processName,
+	}
+
+	if len(daemon.ActiveFlags()) > 0 {
+		d, err := cntxt.Search()
+		if err != nil {
+			log.Fatalln("Unable send signal to the daemon:", err)
+		}
+		daemon.SendCommands(d)
+		return
+	}
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if d != nil {
+		return
+	}
+	defer cntxt.Release()
+
+	log.Println("- - - - - - - - - - - - - - -")
+	log.Println("daemon started")
+
+	go worker()
+
+	err = daemon.ServeSignals()
+	if err != nil {
+		log.Println("Error:", err)
+	}
+	log.Println("daemon terminated")
+}
+
+var (
+	stop = make(chan struct{})
+	done = make(chan struct{})
+)
+
+func termHandler(sig os.Signal) error {
+	log.Println("terminating...")
+	//stop <- struct{}{}
+	if sig == syscall.SIGQUIT || sig == syscall.SIGTERM || sig == os.Interrupt || sig == os.Kill {
+		DB.Close()
+		listener.Close()
+		os.Exit(0)
+	}
+	return daemon.ErrStop
+}
+
+func reloadHandler(sig os.Signal) error {
+	log.Println("configuration reloaded")
+	return nil
 }
