4f76ccacb0071e09cfaf5df336e31c40913d81ea
Denis Barov <dindin@bhyve-host-01.argotech.io>
2016-09-26 09:15:15 +0000

    fix addressing

--- handlers.go
+++ handlers.go
@@ -17,13 +17,13 @@ func processIpmi(body []byte) string {
 	//log.Printf("Command %s received from %s\n", data.command, data.name)
 	switch data.command {
 	case "help":
-		output = help(data.args)
+		output += help(data.args)
 		color = "purple"
 	case "last":
 	case "reboot", "on", "off", "lanboot", "status":
 		switch {
 		case len(data.args) == 0:
-			output = help([]string{data.command})
+			output += help([]string{data.command})
 			color = "purple"
 		default:
 			out = IpmiExec(unwrapAlias(data.args[0], data.name), data.command)
@@ -36,7 +36,7 @@ func processIpmi(body []byte) string {
 		case len(data.args) == 0:
 			aliasEntry := mkDbAliasEntry("", data.name, "0.0.0.0")
 			aliasEntries := showAlias(*aliasEntry)
-			output = fmt.Sprintf("Aliases for you:\n")
+			output += fmt.Sprintf("Aliases for you:\n")
 			for _, a := range aliasEntries {
 				output += aliasToString(a)
 				output += "\n"
@@ -45,43 +45,43 @@ func processIpmi(body []byte) string {
 			aliasEntry := mkDbAliasEntry(data.args[1], data.name, "0.0.0.0")
 			aliasEntries := showAlias(*aliasEntry)
 			if len(aliasEntries) == 0 {
-				output = fmt.Sprintf("There is no alias '%s' for %s\n", data.args[1], data.name)
+				output += fmt.Sprintf("There is no alias '%s' for %s\n", data.args[1], data.name)
 				color = "red"
 				break
 			} else {
 				delAlias(*aliasEntry)
 				aliasEntries := showAlias(*aliasEntry)
 				if len(aliasEntries) == 0 {
-					output = fmt.Sprintf("Alias '%s' removed (owner  %s)", data.args[1], data.name)
+					output += fmt.Sprintf("Alias '%s' removed (owner  %s)", data.args[1], data.name)
 				}
 			}
 		case len(data.args) == 2 && data.args[0] == "show":
 			aliasEntry := mkDbAliasEntry(data.args[1], data.name, "0.0.0.0")
 			aliasEntries := showAlias(*aliasEntry)
 			if len(aliasEntries) == 0 {
-				output = fmt.Sprintf("There is no alias '%s' for %s\n", data.args[1], data.name)
+				output += fmt.Sprintf("There is no alias '%s' for %s\n", data.args[1], data.name)
 				color = "red"
 				break
 			}
-			output = aliasToString(aliasEntries[0])
+			output += aliasToString(aliasEntries[0])
 		case len(data.args) == 3 && data.args[0] == "add":
 			aliasEntry := mkDbAliasEntry(data.args[1], data.name, data.args[2])
 			ip := net.ParseIP(data.args[2])
 			if ip == nil {
-				output = fmt.Sprintf("'%s' doesn't look like IP-address to me\n", data.args[2])
+				output += fmt.Sprintf("'%s' doesn't look like IP-address to me\n", data.args[2])
 				color = "red"
 				break
 			}
 			updateAlias(*aliasEntry)
 			aliasEntries := showAlias(*aliasEntry)
 			if len(aliasEntries) == 0 {
-				output = "Something wrong, I cannot add alias =("
+				output += "Something wrong, I cannot add alias =("
 				color = "red"
 				break
 			}
-			output = aliasToString(aliasEntries[0])
+			output += aliasToString(aliasEntries[0])
 		default:
-			output = help([]string{data.command})
+			output += help([]string{data.command})
 		}
 	}
 
